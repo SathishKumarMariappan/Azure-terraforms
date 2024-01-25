3018e61134d6d298dffe3cb1a6470aa0defecaf0
SathishKumarMariappan <80980539+SathishKumarMariappan@users.noreply.github.com>
2024-01-25 06:39:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "demoforazurejenkinsdeployment"
+  name                     = "20240225639"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
