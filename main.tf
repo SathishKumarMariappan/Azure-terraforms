777d9443b4b436b0e449f7c1927ac21cfb70ab38
SathishKumarMariappan <devsathishkumarm@gmail.com>
2024-07-12 06:23:03 +0530

    d

--- main.tf
+++ main.tf
@@ -3,18 +3,18 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "Demo-resourse"
+  name     = "Demo-resourseafqeg3qg g"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "20240225639"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+# resource "azurerm_storage_account" "example" {
+#   name                     = "20240225639"
+#   resource_group_name      = azurerm_resource_group.example.name
+#   location                 = azurerm_resource_group.example.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
 
-  tags = {
-    environment = "staging"
-  }
-}
+#   tags = {
+#     environment = "staging"
+#   }
+# }
